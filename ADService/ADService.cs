c1932d1a749d93833c320efc5c073f32445249b5
riccardo.iovenitti <riccardo.iovenitti@gmail.com>
2021-06-26 09:05:28 +0200

    * some fixes

--- ADService/ADService.cs
+++ ADService/ADService.cs
@@ -22,14 +22,14 @@ namespace ADService
         private static string SERVICE_NAME = "ADService";
         private static string SERVICE_LOG = "Application";
         private EventLog evLog = null;
-        public const uint GENERIC_READ =    0x80000000;
-        public const uint GENERIC_WRITE =   0x40000000;
-        public const uint DELETE =          0x00010000;
-        public const uint READ_CONTROL =    0x00020000;
-        public const uint WRITE_DAC =       0x00040000;
-        public const uint WRITE_OWNER =     0x00080000;
-        public const uint SYNCHRONIZE =     0x00100000;
-        public const uint READ_ATTRIBUTES = 0x00000080;
+        public const uint DELETE =               0x00010000;
+        public const uint READ_CONTROL =         0x00020000;
+        public const uint WRITE_DAC =            0x00040000;
+        public const uint WRITE_OWNER =          0x00080000;
+        public const uint SYNCHRONIZE =          0x00100000;
+        public const uint FILE_READ_ATTRIBUTES = 0x00000080; 
+        public const uint FILE_READ_EA =         0x00000008; 
+        public const uint FILE_READ_DATA =       0x00000001;
 
         [DllImport("kernel32.dll")]
         static extern bool GetFileSizeEx(IntPtr hFile, out long lpFileSize);
@@ -131,21 +131,16 @@ namespace ADService
             string process = mFilter.GetOriginatorProcessName().ToUpper();
             string fname = e.FileName.ToUpper();
 
-            Console.WriteLine("BeforeRead: {0} by process {1}. GR:{2,5}, A:{3,5}, RC:{4,5}, SY:{5,5}, DA:{6}, OPT:{7}, SM:{8}", fname, process, isGenericRead(e.DesiredAccess), isReadAttributes(e.DesiredAccess), isReadControl(e.DesiredAccess), isSynchronize(e.DesiredAccess), Convert.ToString(e.DesiredAccess, 2).PadLeft(32, '0'), Convert.ToString(e.Options, 2).PadLeft(32, '0'), Convert.ToString(e.ShareMode, 2).PadLeft(32, '0'));
-            
-            /*if (isGenericRead(e.DesiredAccess) && !(isReadControl(e.DesiredAccess) || isReadAttributes(e.DesiredAccess))) {
-                e.ProcessRequest = false;
-                e.ResultCode = (int)ERROR_PRIVILEGE_NOT_HELD;
-                return;
-            }*/
+            //Console.WriteLine("BeforeRead: {0} by process {1}. RD:{2,5}, RA:{3,5}, RC:{4,5}, SY:{5,5}, DA:{6}, OPT:{7}, SM:{8}", fname, process, isGenericRead(e.DesiredAccess), isReadAttributes(e.DesiredAccess), isReadControl(e.DesiredAccess), isSynchronize(e.DesiredAccess), Convert.ToString(e.DesiredAccess, 2).PadLeft(32, '0'), Convert.ToString(e.Options, 2).PadLeft(32, '0'), Convert.ToString(e.ShareMode, 2).PadLeft(32, '0'));
+            Console.WriteLine("BeforeRead: {0} by process {1}. RD:{2,5}, RA:{3,5}, RC:{4,5}, SY:{5,5}", fname, process, isReadData(e.DesiredAccess), isReadAttributes(e.DesiredAccess), isReadControl(e.DesiredAccess), isSynchronize(e.DesiredAccess));
 
-            if (isReadControl(e.DesiredAccess) || isReadAttributes(e.DesiredAccess))
-            {
-                return;
-            }
-         
             // Access to file system alternate streams is always allowed
             if (fname.EndsWith(":ZONE.IDENTIFIER")) return;
+
+            // Reading of attributes, sync or control is always allowed
+            if (!isReadData(e.DesiredAccess) && (isReadControl(e.DesiredAccess) || isReadAttributes(e.DesiredAccess) || isSynchronize(e.DesiredAccess))) {
+                return;
+            }
 
             long fsize = 0;
             try
@@ -154,12 +149,12 @@ namespace ADService
                 bool result = GetFileSizeEx((IntPtr)fHandler, out fsize);
                 if (result)
                 {
-                    Console.WriteLine("File size is: " + fsize);
-                }
-                result = CloseHandle((IntPtr)fHandler);
-                if(!result)
-                {
-                    Console.WriteLine("cannot close file");
+                    //Console.WriteLine("File size is: " + fsize);
+                    result = CloseHandle((IntPtr)fHandler);
+                    if (!result)
+                    {
+                        Console.WriteLine("cannot close file");
+                    }
                 }
             }
             catch(CBFSFilterCbfilterException ex)
@@ -168,22 +163,10 @@ namespace ADService
                 return;
             }
 
-            if (fsize >= 1024)
-            {
-                /*byte[] bytes = new byte[1024];
-                using (var stream = File.OpenRead(fname))
-                {
-                    int count = stream.Read(bytes, 0, 1024);
-                }*/
-                Console.WriteLine("BeforeOpen: " + fname + " >=1K. Process: " + process);
-            }
-            else
-            {
-                Console.WriteLine("BeforeOpen: " + fname + " <1K. Process: " + process);
-            }
-
+            // file is being to be read, only notepad allowed
             e.ResultCode = process.Contains("NOTEPAD.EXE") ? 0 : (int)ERROR_PRIVILEGE_NOT_HELD;
             e.ProcessRequest = (e.ResultCode == 0);
+            Console.WriteLine("Exiting with PR: {0} and RC: {1}.", e.ProcessRequest, e.ResultCode);
         }
         private void onAfterOpenFile(object Sender, CbfilterAfterOpenFileEventArgs e)
         {
@@ -229,17 +212,17 @@ namespace ADService
 
         private bool isReadAttributes(int flag)
         {
-            return (flag & READ_ATTRIBUTES) == READ_ATTRIBUTES;
-        }
-
-        private bool isGenericRead(int flag)
-        {
-            return (flag & GENERIC_READ) == GENERIC_READ;
+            return (flag & FILE_READ_ATTRIBUTES) == FILE_READ_ATTRIBUTES;
         }
 
         private bool isSynchronize(int flag)
         {
             return (flag & SYNCHRONIZE) == SYNCHRONIZE;
+        }
+
+        private bool isReadData(int flag)
+        {
+            return (flag & FILE_READ_DATA) == FILE_READ_DATA;
         }
     }
 }

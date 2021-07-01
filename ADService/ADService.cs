d3622562414f4f33671d00ebe673993c51fe494c
riccardo.iovenitti <riccardo.iovenitti@gmail.com>
2021-07-01 16:08:51 +0200

    * Cleared unused imports

--- ADService/ADService.cs
+++ ADService/ADService.cs
@@ -2,15 +2,11 @@
 using System.Runtime.InteropServices;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Linq;
 using System.ServiceProcess;
 using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using callback.CBFSFilter;
-using System.Threading;
 using adEditor;
 using System.Security.Cryptography;
 using MintPlayer.PlatformBrowser;
